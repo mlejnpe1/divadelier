a851fe8160cb190fe5ed2627d3e60c97b139d4a5
Petr Mlejnek <mlejnpe1@uhk.cz>
2025-09-24 19:38:03 +0200

    production config change

--- backend/src/server.js
+++ backend/src/server.js
@@ -34,10 +34,11 @@ app.use("/api/users", userRoutes);
 app.use("/api/specials", specialRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  const frontendDistPath = path.join(__dirname, "../frontend/dist");
+  app.use(express.static(frontendDistPath));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "..frontend", "dist", "index.html"));
+    res.sendFile(path.join(frontendDistPath, "index.html"));
   });
 }
 
